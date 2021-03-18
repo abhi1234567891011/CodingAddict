2a694a0c22163dfecfc5adea2a36ddf67ed18f83
Abhi <abhi@email.com>
2021-03-18 21:56:45 +0530

    commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+  
+// // const menBtn = document.querySelector('menu-btn');
+// const menBtn = document.getElementById("menu-btn");
+
+// const body = document.querySelector('ul');
+
+
+// menBtn.addEventListener('click' , () => {
+
+//     // body.classList.toggle('showNav');
+//     body.classList.toggle('showNav');
+         
+// })
+
+'use strict'
+
+function abhi(a,b){
+    var abhishek = a + b;
+    console.log(abhishek); 
+}
+
+abhi(10,10);
+
